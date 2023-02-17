532a4ec70121d895edf1472a8668c0b3050eb6fd
Sumit Monapara <sumitmonapara@gmail.com>
2023-02-16 22:53:31 -0500

    EOD Commit

--- src/main/java/com/sumit0733/spring/firstproject/CourseController.java
+++ src/main/java/com/sumit0733/spring/firstproject/CourseController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseController {
 	
-
 	@GetMapping("/courses")
 	public List<Course> getCourses(){
 		 return Arrays.asList(
